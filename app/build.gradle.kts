cb6add50287503162f5444da4e652db79966e1cf
placeboplasma-afk <placeboplasma@gmail.com>
2026-09-23 05:29:39 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,32 +1,4 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
-}
-
-android {
-    namespace = "com.saifulothman.tradingapp"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.saifulothman.tradingapp"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    id("com.android.application") version "8.7.3" apply false
+    kotlin("android") version "2.0.21" apply false
 }
